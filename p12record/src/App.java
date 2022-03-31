cff56f8ed75eb45c8ae61c078fef06d5af528159
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-03-31 16:02:34 +0900

    update
    ・recordの最後にセミコロンが要らなかったため修正
    ・変数spの中身を全て表示するよう変更

--- p12record/src/App.java
+++ p12record/src/App.java
@@ -2,15 +2,16 @@ import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
-        record Word(String english,String japanese,int age,double height){};
+        record Word(String english,String japanese,int age,double height){}
         var w = new Word("英語","日本語",18,33.3);
         System.out.println(w.age + " " + w.english + " " + w);
         record Spending(LocalDate date,int prince,String memo){}
         var sp = new Spending(LocalDate.now(), 500, "ほおほっほほh");
-        System.out.println(sp.date);
+        System.out.println(sp);
         record abc(String a,int b,LocalDate c){}
         var ld = LocalDate.now();
         var abct = new abc("gorigori", 3030,ld);
         System.out.println(abct);
+        
     }
 }
