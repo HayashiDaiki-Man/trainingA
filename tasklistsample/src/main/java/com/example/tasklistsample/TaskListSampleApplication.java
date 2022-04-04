7db5df625a1dab29cd84cbdbc85b350b115a9d5c
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-04 11:16:50 +0900

    add
    spring bootをvscodeで実行するためのファイル

---
+++ tasklistsample/src/main/java/com/example/tasklistsample/TaskListSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklistsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListSampleApplication.class, args);
+	}
+
+}
