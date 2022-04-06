0e918c64a53f076868b07b6967747e4cb5f29f3a
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-06 14:46:38 +0900

    a

--- taskdemo/src/main/java/com/example/taskdemo/TaskListDao.java
+++ taskdemo/src/main/java/com/example/taskdemo/TaskListDao.java
@@ -1,8 +1,9 @@
-package com.example;
-
+package com.example.taskdemo;
+import com.example.taskdemo.HomeController.TaskItem;
 import org.springframework.stereotype.Service;
+import org.springframework.core.JdbcTemplate;
 
 @Service
-public class TaskDao {
-    
+public class TaskListDao {
+    private final  JdbcTemplate jdbcTemplate;
 }
