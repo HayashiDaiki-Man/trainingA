ed3fbc4615bdd77d097a06c3e7ac047f7da09a86
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-21 14:00:51 +0900

    add

--- sample/src/main/java/com/example/demo/model/Invoice.java
+++ sample/src/main/java/com/example/demo/model/Invoice.java
@@ -1,8 +1,17 @@
 package com.example.demo.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +19,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Invoice {
-    
+
     // name
     @NotBlank
     @Size(max = 60)
@@ -23,4 +32,14 @@ public class Invoice {
     @NotBlank
     @Pattern(regexp = "[0-9]*")
     private String phoneNumber;
+    // price
+    @NotNull
+    @Min(1000)
+    @NumberFormat(pattern = "#,###")
+    private BigDecimal price;
+    // paymentDeadline
+    @NotNull
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate paymentDeadline;
+
 }
