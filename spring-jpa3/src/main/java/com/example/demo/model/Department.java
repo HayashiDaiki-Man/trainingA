fddf29682bb710b8f600de558bff55c5f5be4ea5
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-27 16:17:57 +0900

    add

--- spring-jpa3/src/main/java/com/example/demo/model/Department.java
+++ spring-jpa3/src/main/java/com/example/demo/model/Department.java
@@ -2,17 +2,30 @@ package com.example.demo.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+// 部署クラスを作成
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // 4/27 next
+
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
 }
--- spring-jpa3/src/main/java/com/example/demo/model/Employee.java
+++ spring-jpa3/src/main/java/com/example/demo/model/Employee.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+// 社員クラスを作成
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
