ec507749f6b12998bd1459e5cb5febaa697bc2a8
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-27 16:17:34 +0900

    add
    repositoryの作成（employee,department）

---
+++ spring-jpa3/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class EmployeeController {
+    // 4/27 next
+}
