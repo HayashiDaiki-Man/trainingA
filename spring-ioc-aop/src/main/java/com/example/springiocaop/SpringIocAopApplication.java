1176149032233d334d66f2a01757d4a8a79232f7
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-05-03 06:46:37 +0900

    add
    プロジェクトの作成

---
+++ spring-ioc-aop/src/main/java/com/example/springiocaop/SpringIocAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springiocaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocAopApplication.class, args);
+	}
+
+}
